8c1c6f4e47b185236656fcdfefa02f104b0e3494
chieh <ch.wu@northeastern.edu>
2025-10-17 12:39:20 -0400

    kk

---
+++ math_foundation/lecture_12/problem_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import numpy as np
+np.set_printoptions(suppress=True)
+
+
+V = np.array([	[1,2,0],
+				[2,1,0],
+				[0,0,1]])
+
+Զᣳᐟᒾ = np.sqrt(2)
+
+
+U = np.array([	[1/Զᣳᐟᒾ, - 1/Զᣳᐟᒾ , 0	],
+				[1/Զᣳᐟᒾ,   1/Զᣳᐟᒾ , 0	],
+				[0     ,   0      , 1	]])
+
+
