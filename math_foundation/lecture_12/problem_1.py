aecd5233ca756e46647c778592b43ffab62c75bb
chieh <ch.wu@northeastern.edu>
2025-10-17 12:42:55 -0400

    kk

--- math_foundation/lecture_12/problem_1.py
+++ math_foundation/lecture_12/problem_1.py
@@ -4,12 +4,10 @@ import numpy as np
 np.set_printoptions(suppress=True)
 
 
+Զᣳᐟᒾ = np.sqrt(2)
 V = np.array([	[1,2,0],
 				[2,1,0],
 				[0,0,1]])
-
-Զᣳᐟᒾ = np.sqrt(2)
-
 
 U = np.array([	[1/Զᣳᐟᒾ, - 1/Զᣳᐟᒾ , 0	],
 				[1/Զᣳᐟᒾ,   1/Զᣳᐟᒾ , 0	],
